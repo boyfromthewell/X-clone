19071731bbb3938ea88ca18d03e0e3dbfe45a415
SoonyongKwon <tnsdyd10@naver.com>
2023-12-14 16:04:06 +0900

    feat: project, route group setting

---
+++ src/app/(afterLogin)/home/layout.tsx
@@ -0,0 +1,8 @@
+export default async function HomeLayout({ children }) {
+  return (
+    <div>
+      <p>홈 레이아웃</p>
+      {children}
+    </div>
+  );
+}
---
+++ src/app/(afterLogin)/layout.tsx
@@ -0,0 +1,8 @@
+export default function AfterLoginLayout({ children }) {
+  return (
+    <div>
+      애프터 로그인 레이아웃
+      {children}
+    </div>
+  );
+}
